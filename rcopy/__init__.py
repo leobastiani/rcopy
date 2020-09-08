e3f11a54147276814d0a6e72599d356a52713e46
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2020-09-08 11:09:50 -0300

    Fixed

--- rcopy/__init__.py
+++ rcopy/__init__.py
@@ -6,8 +6,21 @@ import sys
 import pyperclip
 from pynput import keyboard
 
+DEBUG = False
+
+def debug(*args):
+    if not DEBUG:
+        return
+    print(*args)
+
+# The key combination to check
+COMBINATIONS = [
+    {keyboard.Key.ctrl_l, keyboard.KeyCode(char='v')},
+    {keyboard.Key.ctrl_l, keyboard.KeyCode(char='V')}
+]
+
 # The currently active modifiers
-current = set()
+pressedButtons = set()
 
 strings = sys.argv[1:]
 i = 0
@@ -19,15 +32,21 @@ def execute():
     if i == len(strings):
         sys.exit(0)
 
-def on_release(key):
+def vk(key):
+    if hasattr(key, 'value'):
+        return key.value.vk
+    return key.vk
+
+def on_press(key):
+    debug("Pressed:", repr(key))
     if hasattr(key, 'char') and key.char == '\x16':
         execute()
+        return
 
 def main():
-    if len(strings) > 0:
-        execute()
-        with keyboard.Listener(on_release=on_release) as listener:
-            listener.join()
+    execute()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 if __name__ == "__main__":
     main()
