52422e94951d39e13de5a774b34a90f9332ae45b
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-03-26 22:32:53 -0300

    add from .

--- rcopy/__init__.py
+++ rcopy/__init__.py
@@ -2,9 +2,9 @@ import platform
 
 def main():
     if platform.system() == 'Darwin':
-        import mac
+        from . import mac
     else:
-        import windows
+        from . import windows
 
 if __name__ == "__main__":
     main()
